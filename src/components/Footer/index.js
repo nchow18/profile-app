466cd1b22bc0a0d7e4d95760dd095c3a9a7bed10
Nathan Chow <emailme@nathanchow.ca>
2021-06-01 00:22:58 -0400

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Icon } from '@iconify/react';
-import githubIcon from '@iconify-icons/logos/github-icon';
-import linkedinIcon from '@iconify-icons/logos/linkedin-icon';
 
 function Footer() {
 
