9b70ff5ee860f7c82915f3d0616bb555fb60689c
Nathan Chow <emailme@nathanchow.ca>
2021-06-30 13:28:50 -0400

    update

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,7 +9,7 @@ function Portfolio() {
     {
       name: 'Day Scheduler',
       link: 'https://nchow18.github.io/work-day-scheduler/',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/main/src/assets/img/0.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/main/src/assets/img/scheduler.png',
       github: 'https://github.com/nchow18/work-day-scheduler',
       label: 'day-schedule',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -17,7 +17,7 @@ function Portfolio() {
     {
       name: 'Envoy',
       link: 'https://envoy-guide.herokuapp.com/',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/main/src/assets/img/1.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/main/src/assets/img/envoy.png',
       github: 'https://github.com/Envoy-products',
       label: 'envoy-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -25,7 +25,7 @@ function Portfolio() {
     {
       name: 'King Seafood',
       link: 'https://king-seafood-2021-1.herokuapp.com/products',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/2.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/king.png',
       github: 'https://github.com/nchow18/king-seafood',
       label: 'king-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -33,7 +33,7 @@ function Portfolio() {
     {
       name: 'Tech Blog',
       link: 'https://tech-blog-2021.herokuapp.com/',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/3.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/tech.png',
       github: 'https://github.com/nchow18/tech-blog',
       label: 'tech-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -41,7 +41,7 @@ function Portfolio() {
     {
       name: 'Mortgages',
       link: 'https://nchow18.github.io/mortgages/',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/4.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/mortgage.png',
       github: 'https://github.com/nchow18/mortgages',
       label: 'mortgages-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -49,7 +49,7 @@ function Portfolio() {
     {
       name: 'Screen Genius',
       link: 'https://screen-genius.github.io/',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/5.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/screen.png',
       github: '',
       label: 'screen-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
@@ -57,7 +57,7 @@ function Portfolio() {
     {
       name: 'Woof',
       link: 'https://woof-2021.herokuapp.com',
-      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/6.png',
+      img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/woof.png',
       github: 'https://github.com/woof-board/woof',
       label: 'woof-port',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed feugiat erat. Praesent in dignissim justo, posuere scelerisque urna. Morbi accumsan lorem at condimentum facilisis. Vestibulum dictum dolor eget consectetur iaculis. Cras at nunc vel metus scelerisque rutrum sit amet sed nulla. Aliquam eu dolor eget eros convallis rutrum. Maecenas blandit dui ex, a fringilla risus fringilla sagittis.'
