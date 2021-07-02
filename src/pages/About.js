aa445c114eed2ed98570d6ea2cf723c04d67fea8
Nathan Chow <emailme@nathanchow.ca>
2021-07-02 14:34:54 -0400

    update pictures

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Photo from '../assets/profile.JPG'
 import Square from '../assets/square-profile.png';
 import '../css/About.css';
 
 function About() {
+
+  const [isModal, setModal] = useState(false)
+
+  const [photos] = useState([]);
+
+  function photosArr() {
+    for (var i = 1; i < 35; i++) {
+      photos.push(i)
+    }
+  }
+
+  useEffect(() => {
+    photosArr();
+  },[])
+
+  const [currentPhoto, setPhoto] = useState(photos[0])
+
+  // process.env.PUBLIC_URL
+
   return (
     <section>
       <div className="about-picture-container">
@@ -14,6 +33,17 @@ function About() {
         <p>I'm currently majoring in Full-stack Coding, as a recent graduate from University of Toronto as a full-stack developer.</p>
         <p>My abilities to work in the Back and Front-end development, helps me work on full projects, from beginning to end.</p>
         <p>This porfolio includes work that I've worked in school, personal projects and clients.</p>
+      </div>
+      <div className="pictures-container">
+        {photos.map((photo) => (
+          <img alt="ig" onClick={() => {setModal(true); setPhoto(photo)}} src={process.env.PUBLIC_URL + `/ig/${photo}.png`}/>
+        ))}
+        {isModal === true && (
+          <div className="pictures-modal">
+            <img alt="ig-modal" onClick={() => {setModal(false)}}src={process.env.PUBLIC_URL + `/ig/${currentPhoto}.png`}/>
+          </div>
+        )}
+
       </div>
     </section>
   )
