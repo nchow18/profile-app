4b56eb3645977713f62a93ca8d0b16ac4b59c406
Nathan Chow <emailme@nathanchow.ca>
2021-07-09 14:54:38 -0400

    archives

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../css/Portfolio.css';
 import ProjectDetails from '../components/Project/ProjectDetails';
 import Auth from '../utils/Auth';
+import Projects from './Projects';
 
 function Portfolio() {
 
@@ -12,7 +13,8 @@ function Portfolio() {
       img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/king.png',
       github: 'https://github.com/nchow18/king-seafood',
       label: 'king-port',
-      details: 'A freshly designed web application for the company Kings Seafood 18, who required a seafood website that can allow customers to view, search and purchase their products, with options of either logging in or as a gueset for storing their items in a shopping cart'
+      details: 'A freshly designed web application for the company Kings Seafood 18, who required a seafood website that can allow customers to view, search and purchase their products, with options of either logging in or as a gueset for storing their items in a shopping cart',
+      year: '2021'
     },
     {
       name: 'Envoy',
@@ -20,7 +22,8 @@ function Portfolio() {
       img: 'https://raw.githubusercontent.com/nchow18/profile-app/feature/new/src/assets/img/envoy.png',
       github: 'https://github.com/Envoy-products',
       label: 'envoy-port',
-      details: 'A web application that allows users to add and comment on environmental products in the market'
+      details: 'A web application that allows users to add and comment on environmental products in the market',
+      year: '2021'
     },
     {
       name: 'Woof',
@@ -29,7 +32,8 @@ function Portfolio() {
       github: 'https://github.com/woof-board/woof',
       label: 'woof-port',
       details: 'A React App that is up to date with the latest trend of e-commerce product delivery.  Consumers can request for a dog walker to walk their dog, and in turn, a registered Dog Walker will be chosen based on their availibility and rating.  The top feature is live tracking of the Dog Walker using Mobile GPS',
-      tech: 'test'
+      tech: 'test',
+      year: '2021'
     }
   ])
 
@@ -41,7 +45,8 @@ function Portfolio() {
       github: 'https://github.com/nchow18/work-day-scheduler',
       label: 'day-schedule',
       details: 'This App allows users to store their current day scheduling, with colour coordinated past, present and future schedules will be displayed based on urgency',
-      tech: 'LocalStorage, API'
+      tech: 'LocalStorage, API',
+      year: '2021'
     },
     {
       name: 'Tech Blog',
@@ -50,7 +55,8 @@ function Portfolio() {
       github: 'https://github.com/nchow18/tech-blog',
       label: 'tech-port',
       details: 'A Tech Blog where users can share, create and update comments to users and forums and discuss about various matters',
-      tech: 'Express-Handlebars, MySQL2, Sequelize, Bcrypt, Dotenv'
+      tech: 'Express-Handlebars, MySQL2, Sequelize, Bcrypt, Dotenv',
+      year: '2021'
     },
     {
       name: 'Mortgages',
@@ -59,7 +65,8 @@ function Portfolio() {
       github: 'https://github.com/nchow18/mortgages',
       label: 'mortgages-port',
       details: 'A Basic mortgage website that displays mortgage information and allows users to sign-up for updates',
-      tech: 'Html, CSS'
+      tech: 'Html, CSS',
+      year: '2021'
     },
     {
       name: 'Screen Genius',
@@ -68,7 +75,8 @@ function Portfolio() {
       github: 'https://screen-genius.github.io/',
       label: 'screen-port',
       details: 'A web based API that allows any user to add, remove and search for movie titles based on genres and ratings',
-      tech: 'LocalStorage, Web API'
+      tech: 'LocalStorage, Web API',
+      year: '2021'
     },
     {
       name: 'Google Book Search',
@@ -78,6 +86,7 @@ function Portfolio() {
       label: 'book-search',
       details: 'This is an app that works with REACT to show how elements are shown with using useState and it is also connected to server side GraphQL database. This displays the uses of Mutations and Queries between client/server and production/development stages. This site allows you to create users, log in, search for books, save and delete books in your profile.',
       tech: 'React, GraphQL Database',
+      year: '2021'
     },
     {
       name: 'Travel Expense Tracker',
@@ -87,6 +96,7 @@ function Portfolio() {
       label: 'expense-tracker',
       details: 'This is an app with the use of indexedDB and service workers, to help increase app functionality with or without internet on your mobile device for tracking your travel expenses.',
       tech: 'IndexedDB, Service Workers',
+      year: '2021'
     },
     {
       name: 'Progressive Web Food App',
@@ -96,6 +106,7 @@ function Portfolio() {
       label: 'progressive-app',
       details: 'An App that is stored on your device as an App, that can be easily accessible without loading a browser independently',
       tech: 'Progressive Web Development, Service Worker',
+      year: '2021'
     },
     {
       name: 'Pizza App',
@@ -105,6 +116,7 @@ function Portfolio() {
       label: 'pizza-app',
       details: 'A web application that helps a pizza business store, create and share pizza orders for purchasing',
       tech: 'MySQL Database',
+      year: '2021'
     },
     {
       name: 'Task Master',
@@ -114,6 +126,7 @@ function Portfolio() {
       label: 'task-master',
       details: 'Web Application to help users locally store their tasks on a device',
       tech: 'LocalStorage',
+      year: '2021'
     },
     {
       name: 'Note Taker',
@@ -123,6 +136,7 @@ function Portfolio() {
       label: 'note-taker',
       details: 'A Web Application to help store and display your current and past notes',
       tech: 'Express.js',
+      year: '2021'
     },
     {
       name: 'Weather Dashboard',
@@ -132,6 +146,7 @@ function Portfolio() {
       label: 'weather-dashboard',
       details: 'A Web Application that searches and displays the 5 day weather forecast for the desired location',
       tech: 'Server Side API',
+      year: '2021'
     },
     // {
     //   name: '',
@@ -145,42 +160,68 @@ function Portfolio() {
   ])
 
   const [currentProject, setCurrentProject] = useState(categories[0]);
+  const [isPortfolioModal, setPortfolioModal] = useState(false)
 
   return (
     <>
-      <section className="project-container">
-        <div>
-          <span>Featured Projects</span>
-          {top.map((link) => (
-            <div className="project-section">
-              <img src={link.img} alt={link.name} onClick={() =>{window.open(link.link)}}/>
-              <div>
-                <span><b>{link.name}</b></span>
-                <p>{link.details}</p>
-                <i className="far fa-folder-open" onClick={() =>{window.open(link.link)}}></i>
-                <i class="fab fa-github" onClick={() =>{window.open(link.github)}}></i>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="others-container">
-          <span>Other Awesome Projects</span>
-          <p onClick={() => {window.location.assign('/projects')}}>View Archive</p>
-          <div className="other-projects">
-            {categories.map((link) => (
-            <div className="others-section">
-              <div>
-                <i className="far fa-folder-open" onClick={() =>{window.open(link.link)}}></i>
-                <i class="fab fa-github" onClick={() =>{window.open(link.github)}}></i>
-              </div>
-                <span><b>{link.name}</b></span>
-                <p>{link.details}</p>
-                <p>{link.tech}</p>
-            </div>
+      {isPortfolioModal ? (
+        <section className="archive-container">
+          <span onClick={() => {setPortfolioModal(false)}}><i className="fas fa-arrow-left"></i>Back to Portfolio</span>
+          <span>Projects Archive</span>
+          <span>A list of small accomplishments</span>
+          <div className="archive-section">
+            <b>Year</b>
+            <b>Title</b>
+            <b>Built With</b>
+            <b>Link</b>
+            {categories.map((category) => (
+              <>
+                <span className="year-text">{category.year}</span>
+                <span className="name-text">{category.name}</span>
+                <span className="tech-text">{category.tech}</span>
+                <span>
+                  <i className="far fa-folder-open" onClick={() =>{window.open(category.link)}}></i>
+                  <i class="fab fa-github" onClick={() =>{window.open(category.github)}}></i>
+                </span>
+              </>
             ))}
           </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <section className="project-container">
+          <div>
+            <span>Featured Projects</span>
+            {top.map((link) => (
+              <div className="project-section">
+                <img src={link.img} alt={link.name} onClick={() =>{window.open(link.link)}}/>
+                <div>
+                  <span><b>{link.name}</b></span>
+                  <p>{link.details}</p>
+                  <i className="far fa-folder-open" onClick={() =>{window.open(link.link)}}></i>
+                  <i class="fab fa-github" onClick={() =>{window.open(link.github)}}></i>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="others-container">
+            <span>Other Awesome Projects</span>
+            <p onClick={() => {setPortfolioModal(true)}}>View Archive</p>
+            <div className="other-projects">
+              {categories.map((link) => (
+              <div className="others-section">
+                <div>
+                  <i className="far fa-folder-open" onClick={() =>{window.open(link.link)}}></i>
+                  <i class="fab fa-github" onClick={() =>{window.open(link.github)}}></i>
+                </div>
+                  <span><b>{link.name}</b></span>
+                  <p>{link.details}</p>
+                  <p>{link.tech}</p>
+              </div>
+              ))}
+            </div>
+          </div>
+        </section>            
+      )}
     </>
 
   );
